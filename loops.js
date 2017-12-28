df64dc3edf8c8dfaac67e06e04e7ec236b0a171e
Lauren Schwartz <lschwartzwriting@gmail.com>
2017-12-28 19:19:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -24,7 +24,7 @@ function doWhileLoop(array){
   return Math.random() >= 0.5;
   }
     do { 
-      array--;
+      array.length--;
   } while (array.length > 0 && maybeTrue());
     return array;
 }
